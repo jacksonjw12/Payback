08a8e0c43db615e1c4444f1830df45e53ab63e67
Jackson Wheeler <jacksonjerrywheeler@gmail.com>
2017-03-04 17:25:54 -0800

    added basic populating from db

--- statics/js/index.js
+++ statics/js/index.js
@@ -1,18 +1,44 @@
 
 
+var populateItems = function(){
+	get('/testReadDB',{},function(data){
+		console.log(data.objs)
+		for(var i = 0; i < data.objs.length; i++){
+			var newEntry = '<div class="col-sm-12 entry"><div class="col-md-8 source"><h3>' + data.objs[i].article.plainText + '</h3><p></p></div><div class="col-md-4 donation text-center"><h4></h4><img><button type="button" class="btn btn-default">Donate Here</button></div></div>';
+
+
+			$('#content').append(newEntry);
+
+		}
+
+	});
+
+}
 
 
 
 
 
 
+populateItems();
 
 
 
 
-
-
-
+function get(domain, obj, callback) {
+	$.ajax({
+		url: domain,
+		type: "GET",
+		data: JSON.stringify(obj),
+		cache: false,
+		dataType: "json",
+		contentType: "application/json"
+	}).done(function (data) {
+		callback(data);
+	}).fail(function () {
+		console.error("Error reading file at '" + domain + "'.");
+	});
+}
 
 
 
