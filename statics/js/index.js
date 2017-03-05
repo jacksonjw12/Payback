b88ca3cff08b2548919d7790e72ffc91ad29ae18
Jackson Wheeler <jacksonjerrywheeler@gmail.com>
2017-03-04 20:28:30 -0800

    may have implemented tweets

--- statics/js/index.js
+++ statics/js/index.js
@@ -10,7 +10,7 @@ var populateItems = function(){
 
 			}
 			else if(data.objs[i].article.type == "twitter"){
-				var newEntry = '<div class="col-sm-12 entry"><div class="col-md-8 source"><h3>' + data.objs[i].article.name + '</h3><blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr"><a href="' + data.objs[i].article.plainText + '"></a></blockquote></div><div class="col-md-4 donation text-center"><h4>' + data.objs[i].charity.name + '</h4><img src="' + data.objs[i].charity.picture + '"><a href="' + data.objs[i].charity.link + '" class="btn btn-default" role="button">Donate Here</a></div></div>';
+				var newEntry = '<div class="col-sm-12 entry"><div class="col-md-8 source"><h3>' + data.objs[i].article.name + '</h3><blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr"><a href="' + data.objs[i].article.twitterID + '"></a></blockquote></div><div class="col-md-4 donation text-center"><h4>' + data.objs[i].charity.name + '</h4><img src="' + data.objs[i].charity.picture + '"><a href="' + data.objs[i].charity.link + '" class="btn btn-default" role="button">Donate Here</a></div></div>';
 
 			}
 
