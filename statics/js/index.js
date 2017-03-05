7d729da137a74938261c1f8e21889da8921500e8
Adam Miller <babaganoosh76@gmail.com>
2017-03-04 23:39:18 -0800

    updated index.js

--- statics/js/index.js
+++ statics/js/index.js
@@ -9,7 +9,7 @@ var populateItems = function(){
 		for(var i = 0; i < data.objs.length; i++){
 			var temp = "Article Title";
 			if(data.objs[i].article.type == "plaintext"){
-				var newEntry = '<div class="col-sm-12 entry"><div class="col-md-8 source"><h3>' + data.objs[i].article.name + '</h3><p>' + data.objs[i].article.plainText + '</p></div><div class="col-md-4 donation text-center"><h4>' + data.objs[i].charity.name + '</h4><img src="' + data.objs[i].charity.picture + '"><a href="javascript:linkClick(' +"'" +data.objs[i].charity.link +"'"+ ')" class="btn btn-default" role="button">Donate Here</a></div></div>';
+				var newEntry = '<div id="div' + (i+1) + '" class="col-sm-12 entry"><div class="col-md-8 source"><h3>' + data.objs[i].article.name + '</h3><p>' + data.objs[i].article.plainText + '</p></div><div class="col-md-4 donation text-center"><h4>' + data.objs[i].charity.name + '</h4><img src="' + data.objs[i].charity.picture + '"><a href="javascript:linkClick(' +"'" +data.objs[i].charity.link +"'"+ ')" class="btn btn-default" role="button">Donate Here</a></div></div>';
 				$('#content').append(newEntry);
 			}
 			else if(data.objs[i].article.type == "twitter"){
@@ -41,8 +41,8 @@ var populateItems = function(){
 
 function linkClick(url){
 	console.log(url);
-	console.log("clicked")
-	window.location = url
+	console.log("clicked");
+	window.location = url;
 }
 
 
