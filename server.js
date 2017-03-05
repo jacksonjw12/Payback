8f828d3649ca765e3126c1fa95c5b2a26a515ca0
Jackson Wheeler <jacksonjerrywheeler@gmail.com>
2017-03-05 09:39:33 -0800

    fixed merge

--- server.js
+++ server.js
@@ -53,19 +53,22 @@ function start() {
 			});  
 			res.send({})
 		}
-		MongoClient.connect(url, function(err, db) {	
+		else{
+			MongoClient.connect(url, function(err, db) {	
 		 
 			
-		  	findPayback(db, function(objs) {
-		  		
+			  	findPayback(db, function(objs) {
+			  		
 
-		  		res.send("ANAND::0 " + objs[n].article.name +  "  " +objs[n].clicks+ "%:1" + objs[n+1].article.name +  "  " +objs[n+1].clicks+ "%:2" + objs[n+2].article.name +  "  " +objs[n+2].clicks+ "%:3" + objs[n+3].article.name +  "  " +objs[n+3].clicks+ "%")
-		  		
+			  		res.send("ANAND::0 " + objs[n].article.name +  "  " +objs[n].clicks+ "%:1" + objs[n+1].article.name +  "  " +objs[n+1].clicks+ "%:2" + objs[n+2].article.name +  "  " +objs[n+2].clicks+ "%:3" + objs[n+3].article.name +  "  " +objs[n+3].clicks+ "%")
+			  		
 
 
-		      	db.close();
-		 	});
-		});  
+			      	db.close();
+			 	});
+			});  
+		
+		}
 		
 		
 
