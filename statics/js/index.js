eeb888a7a59fce9059917f06bf5fd02948099725
Jackson Wheeler <jacksonjerrywheeler@gmail.com>
2017-03-04 20:27:38 -0800

    may have implemented tweets

--- statics/js/index.js
+++ statics/js/index.js
@@ -5,7 +5,14 @@ var populateItems = function(){
 		console.log(data.objs)
 		for(var i = 0; i < data.objs.length; i++){
 			var temp = "Article Title";
-			var newEntry = '<div class="col-sm-12 entry"><div class="col-md-8 source"><h3>' + data.objs[i].article.name + '</h3><p>' + data.objs[i].article.plainText + '</p></div><div class="col-md-4 donation text-center"><h4>' + data.objs[i].charity.name + '</h4><img src="' + data.objs[i].charity.picture + '"><a href="' + data.objs[i].charity.name + '" class="btn btn-default" role="button">Donate Here</a></div></div>';
+			if(data.objs[i].article.type == "plainText"){
+				var newEntry = '<div class="col-sm-12 entry"><div class="col-md-8 source"><h3>' + data.objs[i].article.name + '</h3><p>' + data.objs[i].article.plainText + '</p></div><div class="col-md-4 donation text-center"><h4>' + data.objs[i].charity.name + '</h4><img src="' + data.objs[i].charity.picture + '"><a href="' + data.objs[i].charity.link + '" class="btn btn-default" role="button">Donate Here</a></div></div>';
+
+			}
+			else if(data.objs[i].article.type == "twitter"){
+				var newEntry = '<div class="col-sm-12 entry"><div class="col-md-8 source"><h3>' + data.objs[i].article.name + '</h3><blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr"><a href="' + data.objs[i].article.plainText + '"></a></blockquote></div><div class="col-md-4 donation text-center"><h4>' + data.objs[i].charity.name + '</h4><img src="' + data.objs[i].charity.picture + '"><a href="' + data.objs[i].charity.link + '" class="btn btn-default" role="button">Donate Here</a></div></div>';
+
+			}
 
 
 			$('#content').append(newEntry);
